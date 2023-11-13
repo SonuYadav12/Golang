3bbef7ce2f4364bb1a11ed52957f60313cce87d2
Sonu Kumar Yadav <yadavsonukumar036@gmail.com>
2023-11-13 12:24:06 +0530

    Pointers

--- Pointer.go
+++ Pointer.go
@@ -1,10 +1,33 @@
+// Go has pointers. A pointer holds the memory address of a value.
+
+// The type *T is a pointer to a T value. Its zero value is nil.
+
+// var p *int
+// The & operator generates a pointer to its operand.
+
+// i := 42
+// p = &i
+// The * operator denotes the pointer's underlying value.
+
+// fmt.Println(*p) // read i through the pointer p
+// *p = 21         // set i through the pointer p
+// This is known as "dereferencing" or "indirecting".
+
+// Unlike C, Go has no pointer arithmetic.
+
 package main
 
 import "fmt"
 
-func main(){
-	var p *int32=new(int32)
-	var i int32
-	fmt.Printf("The value of p points to is : %v",*p)
-	fmt.Printf("\nThe value of i is: %v",i)
+func main() {
+	i, j := 42, 2701
+
+	p := &i         // point to i
+	fmt.Println(*p) // read i through the pointer
+	*p = 21         // set i through the pointer
+	fmt.Println(i)  // see the new value of i
+
+	p = &j         // point to j
+	*p = *p / 37   // divide j through the pointer
+	fmt.Println(j) // see the new value of j
 }
